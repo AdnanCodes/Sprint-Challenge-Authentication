5bc228dd1fd3ba4a21e351a616c2f6924ce9f517
Adnan <adnan.chowdhury@outlook.com>
2019-09-20 10:38:04 -0500

    Login endpoint and Token is sent

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -24,6 +24,21 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 function generateToken(user) {
